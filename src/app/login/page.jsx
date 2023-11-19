ea8abfecac5d35c84fa5228c119495190d9108a2
vivi <viviana.calva@lumation.co>
2023-11-19 01:22:00 -0500

    Adding validations to forms

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,7 +1,30 @@
+"use client"
+import { object, string } from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function () {
+  const validationSchema = object().shape({
+    email: string()
+      .email("Ingrese un email válido")
+      .required("Ingrese un email"),
+    password: string()
+      .required("Contraseña requerida"),
+  });
+  const formOptions = {
+    resolver: yupResolver(validationSchema),
+    // mode: "onBlur",
+    mode: "onChange",
+  };
+  const { register, handleSubmit, formState } = useForm(formOptions)
+  const { errors } = formState;
+
+  const handleLogin = (data) => {
+    console.log({ data });
+  }
+
   return (
     <div className="normal-form">
       <section>
@@ -11,15 +34,17 @@ export default function () {
           height={70}
         />
       </section>
-      <form>
+      <form onSubmit={handleSubmit(handleLogin)}>
         <h1 className="title-form">Iniciar sesión</h1>
         <div className="form-item">
-          <label>Usuario</label>
-          <input type="text" key={"user"} />
+          <label>Email</label>
+          <input {...register("email")} type="text" key={"email"} />
+          {errors.email && <span className="validation-error">{errors.email.message}</span>}
         </div>
         <div className="form-item">
           <label>Contraseña</label>
-          <input type="password" key={"password"} />
+          <input {...register("password")} type="password" key={"password"} />
+          {errors.password && <span className="validation-error">{errors.password.message}</span>}
         </div>
         <input className="button-primary" type="submit" value="Iniciar sesión" />
         <div><Link className="link-primary" href={"forgot-password"}>¿Has olvidado tu contraseña?</Link></div>
--- src/app/user/create/page.jsx
+++ src/app/user/create/page.jsx
@@ -1,39 +1,74 @@
+"use client"
+import { object, string } from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import { PASSWORD_REGEX } from "@/constants";
+
 export default function () {
+  const validationSchema = object().shape({
+    name: string()
+      .required("Nombre requerido"),
+    lastname: string().required("Apellido requerido"),
+    email: string()
+      .email("Ingrese un email válido")
+      .required("Email requerido"),
+    password: string()
+      .required("Contraseña requerida")
+      .matches(PASSWORD_REGEX, "La contraseña debe tener al menos 8 caracteres e incluir al menos una letra minúscula, una mayúscula, un número y un carácter especial (!@#$%^&*)"),
+    status: string().is(["active", "inactive", "blocked"], "Estado no válido")
+      .required("Estatus requerido")
+  });
+  const formOptions = {
+    resolver: yupResolver(validationSchema),
+    mode: "onChange",
+  };
+  const { register, handleSubmit, formState } = useForm(formOptions)
+  const { errors } = formState;
+
+  const handleCreateUser = (data) => {
+    console.log({ data });
+  }
+
   return (
     <div className="normal-form">
-      <div className="buttons">
+      {/* <div className="buttons">
         <button>Volver</button>
-        {/* <button>Editar</button> */}
-      </div>
+     <button>Editar</button> 
+    </div> */}
 
-      <form>
+      < form onSubmit={handleSubmit(handleCreateUser)} >
         <h1 className="title-form">Nuevo usuario</h1>
         <div className="form-item">
           <label>Nombre</label>
-          <input type="text" />
+          <input {...register("name")} type="text" />
+          {errors.name && <span className="validation-error">{errors.name.message}</span>}
         </div>
         <div className="form-item">
           <label>Apellido</label>
-          <input type="text" />
+          <input {...register("lastname")} type="text" />
+          {errors.lastname && <span className="validation-error">{errors.lastname.message}</span>}
         </div>
         <div className="form-item">
           <label>Email</label>
-          <input type="email" />
+          <input  {...register("email")} type="email" />
+          {errors.email && <span className="validation-error">{errors.email.message}</span>}
         </div>
         <div className="form-item">
           <label>Contraseña</label>
-          <input type="password" />
+          <input  {...register("password")} type="password" />
+          {errors.password && <span className="validation-error">{errors.password.message}</span>}
         </div>
         <div className="form-item">
           <label>Estado</label>
-          <select>
+          <select {...register("status")} >
             <option value="active" key="active">Activo</option>
             <option value="inactive" key="inactive">Inactivo</option>
             <option value="blocked" key="blocked">Bloqueado</option>
           </select>
+          {errors.status && <span className="validation-error">{errors.status.message}</span>}
         </div>
         <input className="button-primary" type="submit" value="Crear" />
-      </form>
-    </div>
+      </ form>
+    </div >
   );
 }
