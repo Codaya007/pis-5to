012cacef2411b3c435b0ca27451943d3f1973e4e
vivi <viviana.calva@lumation.co>
2024-02-20 19:42:21 -0500

    Adding charts

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,93 +1,25 @@
 'use client';
 import { useState, useEffect } from 'react';
-import { obtener } from '../hooks/Api';
-import { ERROR, LIMIT_PAGINATOR, SUCCESS } from '../hooks/Constants';
-import { alertMessage } from './components/Message';
 import { useAuth } from '@/context/AuthContext';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import Chart from './components/Chart';
+import { getWeatherDataStatics } from '@/services/weatherData.service';
+import { getPronosticsStatics } from '@/services/pronostic.service';
 
-const moment = require('moment-timezone');
-
-const CardByHour = ({ hour, img, weatherType }) => {
-  return (
-    <article className="card">
-      <img src={img || "/Weather.png"} alt={`Weather icon at hour ${hour}`} />
-      <h3>{hour}</h3>
-      <h3>{weatherType}</h3>
-    </article>
-  );
-};
-
-const hoursExample = [
-  {
-    hour: "20:00 pm",
-    image: "",
-  },
-  {
-    hour: "21:00 pm",
-    image: "",
-  },
-  {
-    hour: "22:00 pm",
-    image: "",
-  },
-  {
-    hour: "23:00 pm",
-    image: "",
-  },
-  {
-    hour: "00:00 am",
-    image: "",
-  },
-  {
-    hour: "01:00 am",
-    image: "",
-  },
-];
+const PARAMETER_DICTIONARY = {
+  temperature: "Temperatura",
+  humidity: "Humedad",
+  barometricPressure: "Presión barométrica",
+  windSpeed: "Velocidad del viento"
+}
 
 export default function Home() {
-  const { loginUser, user } = useAuth();
-  const [pronosticos, setPronosticos] = useState({});
-
-  //! Cambiar por el día actual
-  // const fecha = '2024-01-21'
-  const fecha = moment().format('YYYY-MM-DD');
-
-  useEffect(() => {
-    const getPronostics = async () => {
-      let tkn = localStorage.getItem("token");
-      // let tkn = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1Y2Y2NWQ5MjU0OTQ0ZjdhMTg2ZTBjNSIsImlhdCI6MTcwODA5NTM2MSwiZXhwIjoxNzA4NzAwMTYxfQ.Q0v7hwVO5eF13RVkCJ57NjXhuec7jc7AkK6lGi_DEo8"
-
-      let response = await obtener(`pronostics/${fecha}/${fecha}?limit=${LIMIT_PAGINATOR}&page=${1}&populate=${true}`, tkn);
-
-      if (response.msg !== "OK") {
-        alertMessage('Ocurrio un error', pronosticos.msg, ERROR)
-      } else if (response.totalCount == 0) {
-        alertMessage('No hay pronósticos', 'No existen pronósticos en el rango de fechas seleccionado', SUCCESS)
-      } else {
-        response.results.map((element) => {
-          element.dateTime = moment(element.dateTime).format('YYYY-MM-DDTHH:mm:ss[Z]');
-          return element;
-        })
-
-        let diccionario = {}
-
-        response.results.forEach(e => {
-          const fecha = e.dateTime.split('T')[0]
-          diccionario[fecha] = diccionario[fecha] ? [...diccionario[fecha], e] : [e];
-        });
-
-        response.results = diccionario
-
-        console.log({ response });
-
-        setPronosticos(response);
-      }
-    }
-
-    getPronostics()
-
-    //TODO mostar los pronosticos en el carrusel de pronosticos
-  }, []);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [parameter, setParameter] = useState('temperature'); // Por defecto, mostrar temperatura
+  const { loginUser, user, token } = useAuth();
+  const [history, setHistory] = useState([]);
+  const [pronostics, setPronostics] = useState([]);
 
   useEffect(() => {
     if (!user) {
@@ -98,63 +30,72 @@ export default function Home() {
     }
   }, []);
 
+  const handleParameterChange = (newParameter) => {
+    setParameter(newParameter);
+  };
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+    // Lógica para obtener los datos correspondientes a la nueva fecha seleccionada
+  };
+
+  // Lógica para preparar los datos para la gráfica
+  const chartData = {
+    labels: pronostics ? pronostics.map(e => e.label) : [],
+    datasets: [
+      {
+        label: `${PARAMETER_DICTIONARY[parameter]} histórica`,
+        data: history ? history.map(e => e[parameter]) : [],
+        fill: true,
+        // borderColor: 'rgb(75, 192, 192)',
+        color: "pink",
+        tension: 1,
+      },
+      {
+        label: `${PARAMETER_DICTIONARY[parameter]} pronosticada`,
+        data: pronostics ? pronostics.map(e => e[parameter]) : [],
+        fill: true,
+        borderColor: 'purple',
+        tension: 1,
+      },
+    ],
+  };
+
+
+  const fetchData = async () => {
+    const historyData = await getWeatherDataStatics(token, selectedDate);
+    const pronosticData = await getPronosticsStatics(token, selectedDate);
+
+    setPronostics(pronosticData);
+    setHistory(historyData);
+  }
+
+  useEffect(() => {
+    if (token) {
+      fetchData();
+    }
+  }, [selectedDate]);
+
   return (
     <main className="main-container">
-      <section className="items-container">
-        <article className="card main-weather-card">
-          <div>
-            <h3>Loja</h3>
-            <p>Mie, 8 de octubre, 19:00</p>
-          </div>
-          <img src="/Weather.png" alt="Current weather icon" />
-        </article>
-        <div className="params-cards">
-          <article className="card">
-            <h2>Temperatura</h2>
-            <p>30 °C</p>
-          </article>
-          <article className="card">
-            <h2>Humedad</h2>
-            <p>73%</p>
-          </article>
-          <article className="card">
-            <h2>Viento</h2>
-            <p>8 km/h</p>
-          </article>
+      <div>
+        <h1>Comparación de Datos Históricos y Pronósticos</h1>
+        <div>
+          <DatePicker
+            selected={selectedDate}
+            onChange={handleDateChange}
+            maxDate={new Date()} // Solo permitir fechas hasta el día de hoy
+            minDate={new Date('2024/01/01')} // Fecha mínima permitida
+            dateFormat="yyyy/MM/dd"
+          />
         </div>
-        <div className="card params-graph-container">
-          <h2>Historial</h2>
-          <img src="/GraphExample.png" alt="Gráfica de temperatura" />
-          <ul>
-            <li>
-              <button className="active-item">Temperatura</button>
-            </li>
-            <li>
-              <button>Humedad</button>
-            </li>
-            <li>
-              <button>Viento</button>
-            </li>
-          </ul>
+        <div>
+          <button style={{ backgroundColor: parameter === "temperature" ? "skyblue" : "" }} onClick={() => handleParameterChange('temperature')}>Temperatura</button>
+          <button style={{ backgroundColor: parameter === "humidity" ? "skyblue" : "" }} onClick={() => handleParameterChange('humidity')}>Humedad</button>
+          <button style={{ backgroundColor: parameter === "barometricPressure" ? "skyblue" : "" }} onClick={() => handleParameterChange('barometricPressure')}>Presión Barométrica</button>
         </div>
-      </section>
-      {pronosticos && pronosticos.results != null && (
-        <section className="cards-by-hour">
-          {Object.keys(pronosticos.results).map((fecha) => {
-            const data = [...pronosticos.results[fecha]];
-            data.reverse();
-            return (
-              data.map((element, i) => {
-                const hour = element.dateTime.split('T')[1].slice(0, 5);
-                const img = element.pronostic.image;
-                const wType = element.pronostic.weatherType;
-                return <CardByHour hour={hour} img={img} key={i} weatherType={wType} />;
-              })
-            )
-          })
-          }
-        </section>
-      )}
+        <Chart data={chartData} />
+      </div>
     </main>
   );
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -3,8 +3,8 @@ export const IP_REGEX =
 
 export const PASSWORD_REGEX = /^(?=.*[\w\d])(?=.*[\W\S]).{8,}$/;
 
-export const API_BASEURL = "https://api-pis5to.fly.dev";
-// export const API_BASEURL = "http://localhost:3001";
+// export const API_BASEURL = "https://api-pis5to.fly.dev";
+export const API_BASEURL = "http://localhost:3001";
 
 export const UUID_REGEX =
   /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/;
